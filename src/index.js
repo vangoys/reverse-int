6e15b10041f40f1ffe714ca0616282a717366342
vangoys <vanpiskun@gmail.com>
2020-10-25 19:16:09 +0300

    add: function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    
+    if (n < 0) return reverse(-n); 
+    var str = n.toString(); 
+    var strArray = str.split(""); 
+    var revArray = strArray.reverse(); 
+    var rev = revArray.join(""); 
+    return Number(rev);
+  }
